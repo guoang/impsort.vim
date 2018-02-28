f1bd289de68ebf1feb23408795de0c68f01d1f95
Guo Ang <guoang.sysu@gmail.com>
2018-02-28 16:02:18 +0800

    highlight import like: "import aaa.bb"

--- bin/imports.py
+++ bin/imports.py
@@ -2,13 +2,11 @@ from __future__ import print_function, unicode_literals
 
 import os
 import sys
+import jedi
+from jedi import settings
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'jedi'))
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'parso'))
-
-import jedi
-
-from jedi import settings
 
 # Setting a cache for impsort.  It appears that the caches aren't compatible
 # across different versions.
@@ -26,14 +24,24 @@ settings.cache_directory = cache_dir
 def get_names(filename, source):
     # Workaround for forcing an empty function scope on Jedi
     source += '\n\ndef __impsort_fake__():\n    pass\n'
+    script = jedi.Script(source, line=len(source.split('\n')), column=0, path=filename)
 
-    for c in jedi.Script(source, line=len(source.split('\n')), column=0,
-                         path=filename).completions():
+    for c in script.completions():
         if c.name == '__impsort_fake__' or c.is_keyword \
                 or c.in_builtin_module():
             continue
+        if c.type != 'module':
+            print('%s,%s' % (c.name, c.type))
+        elif c.get_line_code().find('from ') != -1:
+            print('%s,%s' % (c.name, c.type))
 
-        print('%s,%s' % (c.name, c.type))
+    for i in script._get_module_node().iter_imports():
+        if i.type == 'import_name':
+            for full, alias in i._dotted_as_names():
+                if alias:
+                    print('%s,%s' % (alias.value, 'module'))
+                else:
+                    print('%s,%s' % ('.'.join([n.value for n in full]), 'module'))
 
 
 if __name__ == '__main__':
